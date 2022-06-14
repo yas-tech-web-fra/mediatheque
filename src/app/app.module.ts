778120bacbebf5eb3f585b3b52fcda914ef09b06
Yas-Tech-Web-Fra <leroy.y1961@gmail.com>
2022-06-14 14:29:55 +0200

    feat : Structure basique du projet :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './_commons/footer/footer.component';
+import { NavBarComponent } from './_commons/nav-bar/nav-bar.component';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    HomePageComponent,
+    ProductPageComponent
   ],
   imports: [
     BrowserModule,
