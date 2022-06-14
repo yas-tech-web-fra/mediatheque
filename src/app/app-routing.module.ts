c9a45c451a62360640bb89f9960021addb14ebe2
Yas-Tech-Web-Fra <leroy.y1961@gmail.com>
2022-06-14 16:32:54 +0200

    Feat : structure, _commons, pages, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './_commons/not-found/not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {  path:'' , redirectTo: 'home', pathMatch: 'full'},
+  {  path:'home' , component: HomePageComponent},
+  {  path:'/products' , component: ProductPageComponent},
+  {  path:'**' , component: NotFoundComponent}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent implements OnInit {
+films: any[]= [];
+
+albums: any[]= [
+  {
+    id: 0,
+    nom: "The dark side of the moon",
+    artiste:"Pink Floyd",
+    annee: 1973,
+    img: "",
+    details:""
+  }
+];
 
   constructor() { }
 
